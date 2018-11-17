17992592da1086c626f00a935e209bcbcff2ed7b
Veronika Ulanova <VU116@ic.ac.uk>
2018-11-17 17:01:31 +0000

    array of values

--- csv_number_extractor.py
+++ csv_number_extractor.py
@@ -6,17 +6,36 @@ Created on Sat Nov 17 14:21:28 2018
 """
 import csv
 import os
+import pandas as pd
 
 
 path = r'D:\Hackathon\Challenges\California\california\train' #change it to your local path 
 directory = os.listdir(path)
 
-def open_file():
-    with open(path + '\\' + 'BG_METADATA_2016.csv') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            print(row['Full_Name'])
-            print (row['Short_Name'])
-    return (reader)  
 
-open_file()
+
+
+def get_code(feature=None):
+    a=0
+    with open(path + '\\' + 'BG_METADATA_2016.csv') as csvfile:
+        info = csv.DictReader(csvfile)
+        for row in info:
+            if row['Full_Name'] == feature: #identifies the short code for the feature we want
+                a = row['Short_Name'] 
+    csvfile.close()             
+    return (a)
+
+def get_data(subset = None):
+    actual_data =[]
+    info = pd.read_csv(path + '\\' + subset)
+    df = pd.DataFrame(info)
+    ide = get_code('SEX BY AGE: Total: Total population -- (Estimate)')
+    for i in df.columns:
+        if i == ide:
+            actual_data = df[i].values
+            #print (actual_data)        
+            #print (len(actual_data))
+    return (actual_data)
+
+get_code('SEX BY AGE: Total: Total population -- (Estimate)')
+get_data('X01_AGE_AND_SEX.csv')
