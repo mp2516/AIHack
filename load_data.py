4b5ef5ff9d17386a72fe643c11842242c190d39f
Robert King <robertkng104@gmail.com>
2018-11-17 16:24:07 +0000

    Plots income

--- load_data.py
+++ load_data.py
@@ -3,22 +3,38 @@ import numpy as np
 import pandas as pd
 import shapefile
 from matplotlib import pyplot as plt
+
 baseDir = 'data/california/california/train'
 
 # meta_data = pd.read_csv(os.path.join(baseDir,'BG_METADATA_2016.csv'))
-# race_data = pd.read_csv(os.path.join(baseDir,'X02_RACE.csv'))
+counts_data = pd.read_csv(os.path.join(baseDir, 'X19_INCOME.csv'))
+print(counts_data.head())
+ca_sf = shapefile.Reader('data/tl_2018_06_tract')
+us_sf = shapefile.Reader('data/tl_2018_us_state')
+x = []
+y = []
+inc = []
+california_shape = us_sf.shape(13)
+x_cali = np.zeros((len(california_shape.points), 1))
+y_cali = np.zeros((len(california_shape.points), 1))
 
-ca_sf = shapefile.Reader('data/cb_2017_06_tract_500k/cb_2017_06_tract_500k')
-print(len(ca_sf.shapes()))
-for i in range(len(ca_sf.shapes())):
-    ca_shape = ca_sf.shape(i)
-    x_data = np.zeros((len(ca_shape.points),1))
-    y_data = np.zeros((len(ca_shape.points),1))
-    for point in range(len(ca_shape.points)):
-        x_data[point] = ca_shape.points[point][0]
-        y_data[point] = ca_shape.points[point][1]
+for i in range(len(california_shape.points)):
+    x_cali[i] = california_shape.points[i][0]
+    y_cali[i] = california_shape.points[i][1]
 
-    plt.plot(x_data,y_data,'k')
+plt.plot(x_cali, y_cali, 'k')
 
+for i in range(int(len(ca_sf.shapes()))):
+    ca_tract_shape = ca_sf.shape(i)
+    GEOID = ca_sf.record(i)[3]
+    tract_df = counts_data[counts_data['GEOID'].str.contains(GEOID, na=True)]
+    if len(tract_df['B19013e1']) == 0:
+        continue
+    income = sum(tract_df['B19013e1']) / len(tract_df['B19013e1'])
+    x.append(float(ca_sf.record(i)[-1]))
+    y.append(float(ca_sf.record(i)[-2]))
+    inc.append(income)
+
+plt.scatter(x, y, c=inc)
 plt.show()
 # print(race_data['GEOID'])
